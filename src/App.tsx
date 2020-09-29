488c3cf50703a9d7da5a5b27c9a35b631d6b1cfa
Henrique Assis <henrique_assis_10@hotmail.com>
2020-09-29 11:55:35 -0300

    sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import React from 'react';
 //STYLE
 import GlobalStyles from './styles/GlobalStyles';
+//COMPONENTS
+import Section from './components/Section';
+//DATA
+import data from './data';
 
 
 function App() {
   return (
     <>
-
-
-    <GlobalStyles />
+      <Section 
+        variant="blue"
+        title={data[0].title}
+        description={data[0].description}
+      />
+      <Section 
+        variant="beige"
+        title={data[1].title}
+        description={data[1].description}
+      />
+      <Section 
+        variant="blue"
+        title={data[2].title}
+        description={data[2].description}
+      />
+      <Section 
+        variant="white"
+        title={data[3].title}
+        description={data[3].description}
+      />
+      <Section 
+        variant="black"
+        title={data[4].title}
+        description={data[4].description}
+      />
+      <GlobalStyles />
     </>
   );
 }
